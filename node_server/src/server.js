53e7f307c7fa258468340de1f1665fa851d13864
yoonhero <yoonhero06@naver.com>
2021-08-08 11:47:40 +0900

    #0.2

--- node_server/src/server.js
+++ node_server/src/server.js
@@ -1,5 +1,7 @@
 import http from "http"
 import express from "express";
+import mongoose from "mongoose";
+
 
 const app = express();
 const PORT = 3000
@@ -7,83 +9,141 @@ const PORT = 3000
 // ranking data 
 // 1 to 100 ranking
 // {username: STRING, score: INT, timestamp: INT}
-let ranking = []
+// let ranking = []
 
 // users array
 //  { username: STRING , highestScore: INT, games: [{ id: INT, score: INT, timestamp: STRING }] }
-let users = []
+// let users = []
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/task-manager", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  highestScore: Number,
+  games: Array,
+});
+
+const RankingSchema = new mongoose.Schema({
+  id: Number,
+  username: String,
+  score: Number,
+})
+
+const User = mongoose.model("User", UserSchema);
+const Ranking = mongoose.model("Ranking", RankingSchema);
 
 function documentation(req, res) {
   res.send('Typing Test for the Developer');
 }
 
-function rankingSystem(data) {
-  let indexOfData = null
-  if (ranking.length === 0) {
-    ranking.push(data)
-    return
-  }
-  ranking.map((record, index) => {
-    if (record.score <= data.score) {
-      indexOfData = indexOfData < index ? indexOfData : index
-    }
-  })
-  if (indexOfData !== null) {
-    let newRanking = []
-    ranking.map((record, index) => {
-      if (index < indexOfData) {
-        newRanking.push(record)
-      }
-    })
-    newRanking.push(data)
-    ranking.map((record, index) => {
-      if (index >= indexOfData) {
-        newRanking.push(record)
-      }
-    })
+async function loadRanking() {
+  return await Ranking.find({})
+}
 
-    if (newRanking.length > 10) {
-      newRanking.pop()
-    }
-    ranking = newRanking
-  } else {
-    ranking.push(data)
-  }
+async function updateRanking(id, data) {
+  const records = new Ranking({
+    id: id,
+    username: data.username,
+    score: data.score
+  });
+  await records.save()
+    .catch((err) => {
+      console.log("Error : " + err);
+    });
+}
+
+async function deleteRanking(id) {
+  await Ranking.findOneAndDelete({ "id": id })
+}
+
+
+async function rankingSystem(data) {
+  let ranking = await loadRanking()
+  updateRanking(ranking.length !== undefined ? ranking.length : 0, data)
 }
 
 // see the 1 to 10 ranking 
-function showRanking(req, res) {
-  res.send(ranking)
+async function showRanking(req, res) {
+  let ranking = await loadRanking()
+  let newRank = ranking.sort(function (a, b) {
+    return b.score - a.score;
+  }).map(function (e, i) {
+    var record = new Object();
+    record.id = e.id
+    record.username = e.username;
+    record.score = e.score;
+    record.rank = i + 1
+    return record;
+  });
+  if (newRank.length > 10) {
+    newRank.map(record => {
+      if (record.rank > 10) {
+        deleteRanking(record.id)
+      }
+    })
+  }
+  res.send(newRank)
+}
+
+async function makeUser(data) {
+  const me = new User({
+    username: data.username,
+    highestScore: data.highestScore,
+    games: data.games
+  });
+  await me.save()
+    .catch((err) => {
+      console.log("Error : " + err);
+    });
+}
+
+async function loadUser(username) {
+  const user = await User.findOne({ "username": username })
+
+  return user
+}
+
+async function updateUser(username, data) {
+  await User.findOneAndUpdate({ "username": username }, { "games": data.games, "highestScore": data.highestScore })
 }
 
 // post ranking data {username, score}
-function addData(req, res) {
+async function addData(req, res) {
   let data = req.body
   if (data === undefined || data === null || data.username === undefined || data.score === undefined || data.timestamp === undefined) {
     res.send({ ok: false })
     return
   }
 
-  let existUser = {}
-  let game = {}
-  let indexOfUser
+  // let indexOfUser
+  // users.map((user, index) => {
+  //   if (user.username === data.username) {
+  //     existUser = user
+  //     indexOfUser = index
+  //     users.splice(index, 1);
+  //     return
+  //   }
+  // })
 
-  users.map((user, index) => {
-    if (user.username === data.username) {
-      existUser = user
-      indexOfUser = index
-      users.splice(index, 1);
-      return
-    }
-  })
+  let existUser = await loadUser(data.username)
 
-  game = {
+  let game = {
     id: existUser?.games?.length !== undefined ? existUser.games.length : 0,
     score: data.score,
-    timestamp: data.timestamp
   }
 
   if (game.id !== 0) {
@@ -91,14 +151,16 @@ function addData(req, res) {
       existUser.highestScore = data.score
     }
     existUser?.games?.push(game)
-    users.push(existUser)
+
+    await updateUser(existUser.username, existUser)
+
   } else {
     existUser = {
       username: data.username,
       highestScore: data.score,
       games: [game]
     }
-    users.push(existUser)
+    await makeUser(existUser)
   }
 
   rankingSystem(data)
@@ -107,15 +169,9 @@ function addData(req, res) {
 }
 
 // get the user's profile
-function userInformation(req, res) {
+async function userInformation(req, res) {
   const { username } = req.params
-  users.map((user) => {
-    if (user.username == username) {
-      res.send({ ok: true, user })
-      return
-    }
-  })
-  res.send({ ok: false })
+  res.send({ ok: false, user: await loadUser(username) })
 
 }
 
@@ -129,3 +185,34 @@ const httpServer = http.createServer(app);
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 httpServer.listen(PORT, handleListen);
+
+
+// OLD RANKING SYSTEM
+// let indexOfData = null
+  // if (ranking.length === 0) {
+  //   ranking.push(data)
+  //   return
+  // }
+  // ranking.map((record, index) => {
+  //   if (record.score <= data.score) {
+  //     indexOfData = indexOfData < index ? indexOfData : index
+  //   }
+  // })\
+// let newRanking = []
+
+//     ranking.map((record, index) => {
+//       if (index < indexOfData) {
+//         newRanking.push(record)
+//       }
+//     })
+//     newRanking.push(data)
+//     ranking.map((record, index) => {
+//       if (index >= indexOfData) {
+//         newRanking.push(record)
+//       }
+//     })
+
+//     if (newRanking.length > 10) {
+//       newRanking.pop()
+//     }
+//     ranking = newRanking
